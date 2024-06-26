4b715298e97e2b2ba7de437d899b01e063f24205
Jonatas <jonatas.melo@3lminformatica.com.br>
2024-06-26 10:52:26 -0300

    doc: documenta as enumerações

--- src/main/java/org/fastsped/commons/Profile.java
+++ src/main/java/org/fastsped/commons/Profile.java
@@ -1,14 +1,52 @@
 package org.fastsped.commons;
 
+/**
+ * Enumeração que representa os perfis de empresa em um arquivo.
+ *
+ * <p>
+ * Este enum define três perfis:
+ * <ul>
+ * <li>{@link #A} - Perfil A.</li>
+ * <li>{@link #B} - Perfil B.</li>
+ * <li>{@link #C} - Perfil C.</li>
+ * </ul>
+ * </p>
+ *
+ * <p>
+ * Cada perfil é representado por uma letra maiúscula ('A', 'B', ou 'C'), indicando
+ * o perfil da empresa ao qual o arquivo está relacionado.
+ * </p>
+ */
 public enum Profile {
-    A("A"), B("B"), C("C");
+    /**
+     * Perfil A.
+     */
+    A("A"),
+    /**
+     * Perfil B.
+     */
+    B("B"),
+    /**
+     * Perfil C.
+     */
+    C("C");
 
     private final String profile;
 
+    /**
+     * Construtor privado do enum Profile.
+     *
+     * @param profile O perfil da empresa representado por uma letra ('A', 'B' ou 'C').
+     */
     Profile(String profile) {
         this.profile = profile;
     }
 
+    /**
+     * Obtém o perfil da empresa como uma string.
+     *
+     * @return A representação do perfil da empresa.
+     */
     public String getProfile() {
         return profile;
     }
